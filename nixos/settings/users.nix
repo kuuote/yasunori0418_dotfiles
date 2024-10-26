2b26675304f6ee047659a8bcab86d846d45ee3bd
kuuote <znmxodq1@gmail.com>
2024-10-27 00:04:20 +0900

    ログインできんのでとりあえず

--- nixos/settings/users.nix
+++ nixos/settings/users.nix
@@ -2,6 +2,7 @@
 {
   users = {
     users.yasunori = {
+      hashedPassword = "$y$j9T$G7oFPasBuVL1NcN3wPu0A/$uNAIdrXb4Kw2RO1s4/BSKpNBTBlywVUHU8wQXfIduz9";
       isNormalUser = true;
       shell = pkgs.zsh;
       description = "yasunori";
